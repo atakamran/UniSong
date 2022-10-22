2c2ea7c6a67d5564d62acc8e3370c255b020e651
Ata Kamran <atakmrn14@gmail.com>
2022-10-22 23:58:22 +0330

    first commit

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,34 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '1a02bd9a72msh12f3b8d415f657dp14dc7ejsnea810076ced5',
+    'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com',
+  },
+};
+
+fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'ShazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set(
+        'X-RAPIDAPI-Key',
+        '1a02bd9a72msh12f3b8d415f657dp14dc7ejsnea810076ced5',
+      );
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: '/charts/world' }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
